805851c1c0f5d58792a43133bcf2e0d2271da929
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2021-12-14 22:04:16 +0700

    Create 320-1.js

---
+++ 320-1.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'e1205d782d8a14eb12c782eb8f4d7b21',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/e1205d782d8a14eb12c782eb8f4d7b21/invoke.js"></scr' + 'ipt>');

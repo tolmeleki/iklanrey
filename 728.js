03a16004ea5277a45b93457273094021da3e491a
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2022-02-03 08:35:19 +0700

    Create 728.js

---
+++ 728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'ff78a8b9f762f105a7b01ef5417f3f4c',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/ff78a8b9f762f105a7b01ef5417f3f4c/invoke.js"></scr' + 'ipt>');

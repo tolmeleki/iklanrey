e667f09c8fdb3bc1dc28ac44e355cff8d8f03ea1
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2022-02-03 08:35:58 +0700

    Update 300-1.js

--- 300-1.js
+++ 300-1.js
@@ -1,8 +1,8 @@
-	atOptions = {
+atOptions = {
 		'key' : '12ef2c2d374b78558e6cc982e1d0f297',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/12ef2c2d374b78558e6cc982e1d0f297/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/12ef2c2d374b78558e6cc982e1d0f297/invoke.js"></scr' + 'ipt>');

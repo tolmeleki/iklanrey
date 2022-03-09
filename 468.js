12575f2caba9b103a87c370dc7e8a649c861081f
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2022-03-09 17:35:06 +0700

    Create 468.js

---
+++ 468.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '8553b82085221bb316cfbd2a4e30f48b',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://motorimperturbable.com/8553b82085221bb316cfbd2a4e30f48b/invoke.js"></scr' + 'ipt>');

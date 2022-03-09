470c28716ee6ace798a9eeac91cf204489866a0b
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2022-03-09 17:31:29 +0700

    Create 160.js

---
+++ 160.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '29c5963d0e9b784f2ade38d48beb7030',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://motorimperturbable.com/29c5963d0e9b784f2ade38d48beb7030/invoke.js"></scr' + 'ipt>');

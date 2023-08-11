5470f02df8978c416af648fbfe7b10b8d3bed67b
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2023-08-11 17:53:51 +0700

    Create 728-2023.js

---
+++ 728-2023.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '2799768b65152fba7efd12f50cdb43c3',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://dispatchoffenderbleat.com/2799768b65152fba7efd12f50cdb43c3/invoke.js"></scr' + 'ipt>');

a222679749596203561e25a487c7dacddb9293d4
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2023-08-11 17:54:27 +0700

    Create 320-2023.js

---
+++ 320-2023.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '042c4183158394c32357d23b92451a50',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://dispatchoffenderbleat.com/042c4183158394c32357d23b92451a50/invoke.js"></scr' + 'ipt>');

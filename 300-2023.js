f55e335e16c721c3c05f2a7c676ef5ee25c3220e
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2023-08-11 17:52:50 +0700

    Create 300-2023.js

---
+++ 300-2023.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9660da32f65849fbab4ba4d8249aeb28',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://dispatchoffenderbleat.com/9660da32f65849fbab4ba4d8249aeb28/invoke.js"></scr' + 'ipt>');

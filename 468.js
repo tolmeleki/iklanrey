b273421491e71ea8e1a977d913a990f5c8c98071
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2024-03-26 12:54:15 +0700

    Update 468.js

--- 468.js
+++ 468.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 468,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://dispatchoffenderbleat.com/8553b82085221bb316cfbd2a4e30f48b/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://topcreativeformat.com/8553b82085221bb316cfbd2a4e30f48b/invoke.js"></scr' + 'ipt>');

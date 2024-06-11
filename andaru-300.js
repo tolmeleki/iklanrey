f734715322669e022ecf76d36507671b155c4976
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2024-06-11 21:52:16 +0700

    Create andaru-300.js

---
+++ andaru-300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '1d91ee5ca1cdce28625c14cec713bef1',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://restlesscompeldescend.com/1d91ee5ca1cdce28625c14cec713bef1/invoke.js"></scr' + 'ipt>');

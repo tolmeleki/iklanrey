2cab87293c758a6c5e39563ec7cd2432c2fc38c5
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2024-07-30 21:21:40 +0700

    Update 320-1.js

--- 320-1.js
+++ 320-1.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 320,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://topcreativeformat.com/97264edc4c73fd2d5b41ac5081114f10/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hornstaughtsession.com/97264edc4c73fd2d5b41ac5081114f10/invoke.js"></scr' + 'ipt>');

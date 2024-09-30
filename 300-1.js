70f5709bfe70bffa4baf8acf9218fb174ce33b6e
tolmeleki <67069578+tolmeleki@users.noreply.github.com>
2024-09-30 22:39:38 +0700

    Update 300-1.js

--- 300-1.js
+++ 300-1.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hornstaughtsession.com/768320f2a9b589040780a7f24f18f88e/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://gullbastardtorment.com/768320f2a9b589040780a7f24f18f88e/invoke.js"></scr' + 'ipt>');
